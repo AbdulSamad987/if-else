b8b5c6a581982e165ae05198c995ff133aae74f3
AbdulSamad987 <techgurusammubadshahshorts@gmail.com>
2024-12-16 19:28:28 +0500

    newsave

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-let username = "abdulsamad@gmail.com"
-if (username === "abdulsamad@gmail.com") {
-    console.log("if executed")
-    console.log(`Variable name is: abdulsamad@gmail.com`);
+//let username = "abdulsamad765@gmail.com"
+//if (username === "abdulsamad@gmail.com") {
+//console.log("if executed")
+//console.log(`Variable name is: abdulsamad@gmail.com`);
+//}
+//else {
+// console.log("else executed")
+//console.log(`Variable name is: ${username} but it is not abdulsamad@gmail.com`)
+//}
+
+//Target 2
+//let age = parseInt(prompt("enter a age"));
+//if (age >= 18) {
+//  console.log(` ${age} are eligible`)
+//}
+//else {
+//  console.log(` ${age} are not eligible`)
+
+//}
+//complete
+
+//Task 3
+
+
+
+//let chks
+let task3 = "Goodmorning";
+if (task3 >= 5 && task3 <= 12) {
+    task3 = "Goodmorning";
+} else if (task3 >= 12 && task3 <= 18) {
+    task3 = "Good afternoon";
+} else {
+    task3 = "Good evening";
 }
-else {
-    console.log("else executed")
-    console.log(`Variable name is: ${username} but it is not abdulsamad@gmail.com`)
-}
+console.log(task3);
+// console.log(chks);
