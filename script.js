f8164afd75f67e62fbd75cbf0cd7b19494e9a7dc
AbdulSamad987 <techgurusammubadshahshorts@gmail.com>
2024-12-16 16:50:04 +0500

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+let username = "abdulsamad@gmail.com"
+if (username === "abdulsamad@gmail.com") {
+    console.log("if executed")
+    console.log(`Variable name is: abdulsamad@gmail.com`);
+}
+else {
+    console.log("else executed")
+    console.log(`Variable name is: ${username} but it is not abdulsamad@gmail.com`)
+}
